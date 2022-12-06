f441b7c04de5d620d4025a136876aef49f162378
jin hye <sjrry112@gmail.com>
2022-12-06 19:21:04 +0900

    1206_진혜_3rd_api검색창찾음

---
+++ test-project/src/components/ApiLandingPage.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import ApiMapContainer from "./ApiMapcontainer";
+import "../css/ApiLandingPage.css";
+function ApiLandingPage() {
+  const [InputText, setInputText] = useState("");
+  const [Place, setPlace] = useState("");
+
+  const onChange = e => {
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setPlace(InputText);
+    setInputText("");
+  };
+
+  return (
+    <>
+      <ApiMapContainer searchPlace={Place} />
+      <form
+        className="api-inputForm"
+        onSubmit={handleSubmit}
+        style={{
+          position: "relative",
+          width: "100%",
+          bottom: "820px",
+        }}
+      >
+        <input
+          className="api-input"
+          placeholder="검색어를 입력하세요"
+          onChange={onChange}
+          value={InputText}
+          style={{
+            display: "inline-block",
+            border: "none",
+            borderBottom: "1px solid green",
+            fontSize: "20px",
+          }}
+        />
+        <button type="submit">검색</button>
+      </form>
+    </>
+  );
+}
+
+export default ApiLandingPage;
