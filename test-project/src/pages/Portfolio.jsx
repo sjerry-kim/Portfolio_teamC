3a417de8998f875b9d5281c347b66d69c688c97f
hjk92go <hjk92go@gmail.com>
2022-12-07 16:18:52 +0900

    221207_현주_2nd_commit_Portfolio수정 git commit -m 221207_현주_2nd_commit_Portfolio수정
    git commit -m 221207_현주_2nd_commit_Portfolio수정

--- test-project/src/pages/Portfolio.jsx
+++ test-project/src/pages/Portfolio.jsx
@@ -1,51 +1,213 @@
-import { } from "../css/ProductSlider.css";
+import { Link } from "react-router-dom";
+import {} from "../css/Portfolio.css";
 
-const Slider = () => {
-
-
-  const sliderData = [
-    { labelNum: "s1", sliderNum: "slider1", title: "업체1", desc: "설명1", img: "house01.jpg" },
-    { labelNum: "s2", sliderNum: "slider2", title: "업체2", desc: "설명2", img: "house02.jpg" },
-    { labelNum: "s3", sliderNum: "slider3", title: "업체3", desc: "설명3", img: "house03.jpg" },
-    { labelNum: "s4", sliderNum: "slider4", title: "업체4", desc: "설명4", img: "house04.jpg" },
-    { labelNum: "s5", sliderNum: "slider5", title: "업체5", desc: "설명5", img: "house05.jpg" },
-    { labelNum: "s6", sliderNum: "slider6", title: "업체6", desc: "설명6", img: "house06.jpg" },
-    { labelNum: "s7", sliderNum: "slider7", title: "업체7", desc: "설명7", img: "house07.jpg" }
-  ];
-
+const Portfolio = () => {
   return (
     <div>
-      <div style={{ width: "100%", height: "150px", backgroundColor: "yellow" }}></div>
-      <di className="slider-body">
-        <div className="slider-container">
-          <input type="radio" name="slider" className="d-none" id="s1" />
-          <input type="radio" name="slider" className="d-none" id="s2" />
-          <input type="radio" name="slider" className="d-none" id="s3" />
-          <input type="radio" name="slider" className="d-none" id="s4" />
-          <input type="radio" name="slider" className="d-none" id="s5" />
-          <input type="radio" name="slider" className="d-none" id="s6" />
-          <input type="radio" name="slider" className="d-none" id="s7" />
-          <div className="slider-cards">
-            {sliderData.map((sliderData, index) => {
-              <label key={index} for={sliderData.labelNum} id={sliderData.sliderNum}>
-                <div className="slider-card">
-                  <div className="slider-image">
-                    <img src={require(`../img/${sliderData.img}`)} />
-                  </div>
-                  <div className="slider-infos">
-                    <span>{sliderData.title}</span>
-                    <span>{sliderData.desc}</span>
-                  </div>
+      <body className="portfolio-body">
+        <div className="portfolio-container">
+          <input
+            type="radio"
+            name="slider"
+            className="portfolio-d-none"
+            id="s1"
+          />
+          <input
+            type="radio"
+            name="slider"
+            className="portfolio-d-none"
+            id="s2"
+          />
+          <input
+            type="radio"
+            name="slider"
+            className="portfolio-d-none"
+            id="s3"
+          />
+          <input
+            type="radio"
+            name="slider"
+            className="portfolio-d-none"
+            id="s4"
+          />
+          <input
+            type="radio"
+            name="slider"
+            className="portfolio-d-none"
+            id="s5"
+          />
+          <input
+            type="radio"
+            name="slider"
+            className="portfolio-d-none"
+            id="s6"
+          />
+          <input
+            type="radio"
+            name="slider"
+            className="portfolio-d-none"
+            id="s7"
+          />
+          <input
+            type="radio"
+            name="slider"
+            className="portfolio-d-none"
+            id="s8"
+          />       
+          <input
+            type="radio"
+            name="slider"
+            className="portfolio-d-none"
+            id="s9"
+          />               
+          
+
+          <div className="portfolio-cards">
+            <label for="s1" id="portfolio-slide1">
+              <div className="portfolio-card">
+                <div className="portfolio-image">
+                  <img src="img/Nike SuperRep Go.jpg" alt="" />
                 </div>
-              </label>
-            })}
+                <div className="portfolio-infos">
+                  <span className="portfolio-name">1번째 카드</span>
+                  <span className="portfolio-lorem">
+                    Lorem ipsum dolor sit amet, sit amet adipiscing elit. Aenean
+                    vel ansd . Nullam lorem. Nulla eu sodales
+                  </span>
+
+                  <button>
+                    <Link to="/product">product</Link>
+                  </button>
+                </div>
+              </div>
+            </label>
+
+            <label for="s2" id="portfolio-slide2">
+              <div className="portfolio-card">
+                <div className="portfolio-image">
+                  <img src="img/Free run flyknit.jpg" alt="" />
+                </div>
+                <div className="portfolio-infos">
+                  <span className="portfolio-name">2번째 카드</span>
+                  <span className="portfolio-lorem">
+                    Lorem ipsum dolor sit, adipiscing elit. Aenean vel sit ansd
+                    . Nullam lorem. Nulla eu sodales karma stellus
+                  </span>
+                </div>
+              </div>
+            </label>
+
+            <label for="s3" id="portfolio-slide3">
+              <div className="portfolio-card">
+                <div className="portfolio-image">
+                  <img src="img/Black Toe Union.jpg" alt="" />
+                </div>
+                <div className="portfolio-infos">
+                  <span className="portfolio-name">3번째 카드</span>
+                  <span className="portfolio-lorem">
+                    Lorem ipsum dolor, sit amet let kar adipiscing. Aenean vel
+                    velit sit ansd . Nullam lorem. Nulla karma stellus
+                  </span>
+                </div>
+              </div>
+            </label>
+
+            <label for="s4" id="portfolio-slide4">
+              <div className="portfolio-card">
+                <div className="portfolio-image">
+                  <img src="img/Retro High Og.jpg" alt="" />
+                </div>
+                <div className="portfolio-infos">
+                  <span className="portfolio-name">4번째 카드</span>
+                  <span className="portfolio-lorem">
+                    Lorem ipsum dolor sit, sit amet elit. Aenean sit amet sit
+                    amet vel velit sit ansd. Nulla eu sodales stellus
+                  </span>
+                </div>
+              </div>
+            </label>
+
+            <label for="s5" id="portfolio-slide5">
+              <div className="portfolio-card">
+                <div className="portfolio-image">
+                  <img src="img/Off-White x Air Jordan.jpg" alt="" />
+                </div>
+                <div className="portfolio-infos">
+                  <span className="portfolio-name">5번째 카드</span>
+                  <span className="portfolio-lorem">
+                    Lorem ipsum sit amet, sit amet elit. Aenean vel velit ansd .
+                    Nullam lorem. Nulla eu sodales stellus
+                  </span>
+                </div>
+              </div>
+            </label>
+
+            <label for="s6" id="portfolio-slide6">
+              <div className="portfolio-card">
+                <div className="portfolio-image">
+                  <img src="img/Off-White x Air Jordan.jpg" alt="" />
+                </div>
+                <div className="portfolio-infos">
+                  <span className="portfolio-name">6번째 카드</span>
+                  <span className="portfolio-lorem">
+                    Lorem ipsum sit amet, sit amet elit. Aenean vel velit ansd .
+                    Nullam lorem. Nulla eu sodales stellus
+                  </span>
+                </div>
+              </div>
+            </label>
+
+            <label for="s7" id="portfolio-slide7">
+              <div className="portfolio-card">
+                <div className="portfolio-image">
+                  <img src="img/Off-White x Air Jordan.jpg" alt="" />
+                </div>
+                <div className="portfolio-infos">
+                  <span className="portfolio-name">7번째 카드</span>
+                  <span className="portfolio-lorem">
+                    Lorem ipsum sit amet, sit amet elit. Aenean vel velit ansd .
+                    Nullam lorem. Nulla eu sodales stellus
+                  </span>
+                </div>
+              </div>
+            </label>
+
+
+            <label for="s8" id="portfolio-slide8">
+              <div className="portfolio-card">
+                <div className="portfolio-image">
+                  <img src="img/Off-White x Air Jordan.jpg" alt="" />
+                </div>
+                <div className="portfolio-infos">
+                  <span className="portfolio-name">새로 추가한 8번째 카드</span>
+                  <span className="portfolio-lorem">
+                    Lorem ipsum sit amet, sit amet elit. Aenean vel velit ansd .
+                    Nullam lorem. Nulla eu sodales stellus
+                  </span>
+                </div>
+              </div>
+            </label>
+
+            <label for="s9" id="portfolio-slide9">
+              <div className="portfolio-card">
+                <div className="portfolio-image">
+                  <img src="img/Off-White x Air Jordan.jpg" alt="" />
+                </div>
+                <div className="portfolio-infos">
+                  <span className="portfolio-name">새로 추가한 9번째 카드</span>
+                  <span className="portfolio-lorem">
+                    Lorem ipsum sit amet, sit amet elit. Aenean vel velit ansd .
+                    Nullam lorem. Nulla eu sodales stellus
+                  </span>
+                </div>
+              </div>
+            </label>
+
           </div>
         </div>
-      </di>
+      </body>
     </div>
-
-
-  )
+  );
 };
 
-export default Slider;
+export default Portfolio;
