c089d3478efea06a8557b49866951df31f6819ec
jinhye <sjrry112@gmail.com>
2022-12-09 00:07:59 +0900

    1208_진혜_3rd_새로고침뒤로가기추가

--- test-project/src/components/Navbar.jsx
+++ test-project/src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 import DataContext from "../data/DataContext";
 
 const Navbar = () => {
-  const { state } = useContext(DataContext);
+  const { action } = useContext(DataContext);
 
   return ( 
     <div style={{height:" 100px", backgroundColor:"transparent"}}>
       <Link to='/'>today design</Link>
       <Link to='/main/portfolio'>Portfolio</Link>
-      <Link to='/main/estimation' onClick={()=>{state.reciept([])}}>Estimation</Link>
+      <Link to='/main/estimation' onClick={()=>{action.setReciept([]);}}>Estimation</Link>
     </div>
   );
 }
