c18d8d6596a590036f4d1c43e9d748fa8e5505ef
정회윤 <107393920+junghoeyun@users.noreply.github.com>
2022-12-12 12:42:46 +0900

    수정

--- test-project/src/App.js
+++ test-project/src/App.js
@@ -1,35 +1,36 @@
-import './App.css';
-import {Route, Routes} from 'react-router-dom';
-import Home from './pages/Home';
-import Main from './pages/Main';
-import EstStart from './components/EstStart';
-import Estimation from './pages/Estimation';
-import Portfolio from './pages/Portfolio';
-import ProductMergePage from './components/ProductMergePage'
-import Nonfound from './pages/Notfound';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Main from "./pages/Main";
+import EstStart from "./components/EstStart";
+import Estimation from "./pages/Estimation";
+import Portfolio from "./pages/Portfolio";
+import ProductMergePage from "./components/ProductMergePage";
+import Nonfound from "./pages/Notfound";
 
-import { DataProvider } from './data/DataContext'
-import EstQuestion from './components/EstQuestion';
-import EstResult from './components/EstResult';
-import EstReciept from './components/EstReciept';
-
+import { DataProvider } from "./data/DataContext";
+import EstQuestion from "./components/EstQuestion";
+import EstResult from "./components/EstResult";
+import EstReciept from "./components/EstReciept";
+import ApiLandingPage from "./components/ApiLandingPage";
 
 function App() {
   return (
     <div className="App">
       <DataProvider>
         <Routes>
-          <Route index path='/' element={<Home />}/>
-          <Route path='/main' element={<Main />}>
-            <Route index  element={<Portfolio />}/>
-            <Route path='/main/portfolio' element={<Portfolio />} />
-            <Route path='/main/estimation' element={<EstStart /> }/>
+          <Route index path="/" element={<Home />} />
+          <Route path="/main" element={<Main />}>
+            <Route index element={<Portfolio />} />
+            <Route path="/main/portfolio" element={<Portfolio />} />
+            <Route path="/main/estimation" element={<EstStart />} />
             <Route path="/main/question" element={<EstQuestion />} />
             <Route path="/main/result" element={<EstResult />} />
             <Route path="/main/reciept" element={<EstReciept />} />
-            <Route path='/main/product' element={<ProductMergePage />} />
+            <Route path="/main/product" element={<ProductMergePage />} />
+            <Route path="/main/map" element={<ApiLandingPage />} />
           </Route>
-          <Route path='*'element={<Nonfound />}/>
+          <Route path="*" element={<Nonfound />} />
         </Routes>
       </DataProvider>
     </div>
--- test-project/src/components/HomeContactUs.jsx
+++ test-project/src/components/HomeContactUs.jsx
@@ -1,15 +1,29 @@
-import { Button, Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import "../css/HomeContactUs.css";
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const HomeContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = e => {
+    e.preventDefault();
+    const Service_ID = "service_y4raiv3";
+    const Template_ID = "template_sqorybn";
+    const User_ID = "odmr0vvUUkxqQq_1d";
+
+    emailjs.sendForm(Service_ID, Template_ID, form.current, User_ID).then(
+      result => alert(result.text),
+      error => alert(error.text)
+    );
+    e.currentTarget.reset();
+  };
   return (
     <div>
       <h2 className="homecontactus-title">Contact Us</h2>
       <div>
-          <button className="homecontactus-button">
-            업체바로가기
-          </button>
-        </div>
+        <button className="homecontactus-button">업체바로가기</button>
+      </div>
       <div className="homecontactus-letter">
         <div className="homecontactus-middletitle">
           <p>주소주소주소주소주소주소</p>
@@ -18,8 +32,7 @@ const HomeContactUs = () => {
           <p>이메일:GamilC@gmail.com</p>
         </div>
       </div>
-
-      <form className="homecontactus-form">
+      <form className="homecontactus-form" ref={form} onSubmit={sendEmail}>
         <div className="homecontactus-formdiv">
           <label className="homecontactus-formlable">회사 이름</label>
           <input className="homecontactus-forminput" type="text"></input>
@@ -47,7 +60,9 @@ const HomeContactUs = () => {
         </div>
 
         <div>
-          <Button variant="btn btn-outline-light">보내기</Button>
+          <Button variant="btn btn-outline-light" type="submit">
+            보내기
+          </Button>
         </div>
       </form>
     </div>
