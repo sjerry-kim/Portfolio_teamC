5b9745440b49a68f28638e04a033996830785d93
jinhye <sjrry112@gmail.com>
2022-12-30 15:21:23 +0900

    1230_진혜_마이페이지ㅗ

--- test-project/src/components/MyPageAccount.jsx
+++ test-project/src/components/MyPageAccount.jsx
@@ -1,4 +1,4 @@
-import '../css/MyPageAccount.css';
+import "../css/MyPageAccount.css";
 import { async } from "@firebase/util";
 import { updateProfile } from "firebase/auth";
 import { useEffect } from "react";
@@ -9,16 +9,21 @@ import MyPageProfileModal from "./MyPageProfileModal";
 
 const MyPageAccount = () => {
   const [update, setUpdate] = useState();
-  useEffect(()=>{},[update])
+  useEffect(() => {}, [update]);
 
   const user = auth.currentUser;
 
-  const sessionFireUser =  window.sessionStorage.getItem("firebase:authUser:AIzaSyCMoXUqkehoGjCep79k-dmXJLJfb-HZuFo:[DEFAULT]")
+  const sessionFireUser = window.sessionStorage.getItem(
+    "firebase:authUser:AIzaSyCMoXUqkehoGjCep79k-dmXJLJfb-HZuFo:[DEFAULT]"
+  );
   const parseSFU = JSON.parse(sessionFireUser);
+  const name = parseSFU.displayName;
+  const profile = parseSFU.photoURL;
+  const email = parseSFU.email;
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log(parseSFU.photoURL);
-  },[])
+  }, []);
   // const [displayName, setDisplayName] = useState("");
   // const [email, setEmail] = useState("");
   // const [password, setPassword] = useState("");
@@ -33,7 +38,7 @@ const MyPageAccount = () => {
   //   console.log(displayName)
   //   console.log(email);
   // }
-  
+
   // const changeUserName = () => {
   //   updateProfile(auth.currentUser, {
   //     displayName: "카카"
@@ -44,71 +49,55 @@ const MyPageAccount = () => {
   //   });
   // }
 
-  return (  
-    <div className='mypageaccount-wallpaper'>
-      <div className='mypateaccount-div'>
-      <h1 className='mypageaccount-title'>My Page</h1>
-      {
-        window.sessionStorage.getItem("googleLogin") === "true"?  (
-          <div className='mypageaccount-img-div'>
-            <img className='mypageaccount-img'
-              src={parseSFU.photoURL}
+  return (
+    <div className="mypageaccount-wallpaper">
+      <div className="mypateaccount-div">
+        <h1 className="mypageaccount-title">My Page</h1>
+        {window.sessionStorage.getItem("googleLogin") === "true" ? (
+          // 구글계정으로 로그인 했을 때
+          <div className="mypageaccount-img-div">
+            <img
+              className="mypageaccount-img"
+              src={profile}
               alt="이미지 없음"
-              />
-              {/* <div style={{
-                            width:"150px", 
-                            height:"150px",
-                            backgroundImage:`${window.sessionStorage.getItem("photoURL")}`, 
-                            backgroundColor: "white",
-                            backgroundSize: "cover",
-                            borderRadius:"50%"}}>
-              </div> */}
+            />
           </div>
-        ):(
-          <div className='mypageaccount-img-div'>
-            {/* <div
-              className='mypageaccount-img'
-              style={{
-                            width:"100px", 
-                            height:"100px",
-                            backgroundImage:`url(${window.sessionStorage.getItem("photoURL")})`, 
-                            backgroundSize: "cover",
-                            borderRadius:"50%"}}>
-              </div>
-              <MyPageProfileModal /> */}
-              {/* <div
-                className='mypageaccount-img'
-                style={{
-                            width:"100px", 
-                            height:"100px",
-                            backgroundImage:`url(${require(`${window.sessionStorage.getItem("photoURL")}`)})`, 
-                            backgroundColor: "white",
-                            backgroundSize: "cover",
-                            borderRadius:"50%"}}>
-              </div> */}
-              <img className='mypageaccount-img'
-                src={parseSFU.photoURL} alt="이미지 없음" />
+        ) :  //이메일, 비번으로 로그인 했을 때
+        window.sessionStorage.getItem("profileClick") == "false"? (
+          <div className="mypageaccount-img-div">
+            <img
+              className="mypageaccount-img"
+              src={require("../img/defaultProfile.jpeg")}
+              alt="이미지 없음"
+            />
             <MyPageProfileModal setUpdate={setUpdate} />
           </div>
-        )
-      }
-      {/* <MyPageProfileModal /> */}
-      <div className='mypageaccount-info-div'>
-        <div className='mypageaccount-info-div-box'>
-        <label style={{paddingRight:"14px"}}>Name</label>
-        <input type="text" value={window.sessionStorage.getItem("displayName")} disabled />
+        ) : (
+          <div className="mypageaccount-img-div">
+            <img
+              className="mypageaccount-img"
+              src={profile}
+              alt=" no img "
+            />
+            <MyPageProfileModal setUpdate={setUpdate} />
+          </div>
+          )
+        }
+        {/* <MyPageProfileModal /> */}
+        <div className="mypageaccount-info-div">
+          <div className="mypageaccount-info-div-box">
+            <label style={{ paddingRight: "14px" }}>Name</label>
+            <input type="text" value={name} disabled />
+          </div>
+          <div className="mypageaccount-info-div-box">
+            <label>E-mail</label>
+            <input type="text" value={email} disabled />
+          </div>
         </div>
-        <div className='mypageaccount-info-div-box'>
-        <label>E-mail</label>
-        <input type="text" value={window.sessionStorage.getItem("email")} disabled />
-        </div>
       </div>
-      </div>
-      <div className='mypageaccount-recentcomment-div'>
-        
-      </div>
+      <div className="mypageaccount-recentcomment-div"></div>
     </div>
   );
-}
+};
 
 export default MyPageAccount;
