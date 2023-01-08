dba686c0ba729bf162c1b292af2d4221214f2e9c
jinhye <sjrry112@gmail.com>
2023-01-08 18:08:51 +0900

    0108_진혜_진희언니 홈 모바일 버전 수정 완

--- test-project/src/pages/Home.jsx
+++ test-project/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import "../css/Home.css"
+import "../css/Home.css";
 import HomeAboutUs from "../components/HomeAboutUs";
 import HomePortfolio from "../components/HomePortfolio";
 import HomeContactUs from "../components/HomeContactUs";
@@ -29,19 +29,19 @@ const Home = () => {
           touchMove: false,
           onSwipe: false,
           draggable: false,
-        }
+        },
       },
-    ]
+    ],
   };
 
   const slider = useRef(null);
-  const page3 = useRef(null);
-  const scrollToSection = (elementRef) => {
-    window.scrollTo({
-      top: elementRef.current.offsetTop,
-      behavior: "smooth",
-    });
-  };
+  // const page3 = useRef(null);
+  // const scrollToSection = (elementRef) => {
+  //   window.scrollTo({
+  //     top: elementRef.current.offsetTop,
+  //     behavior: "smooth",
+  //   });
+  // };
 
   function scroll(e) {
     if (slider === null) return 0;
@@ -55,67 +55,92 @@ const Home = () => {
     };
   }, []);
 
-//   useEffect(()=>{
-//   let slider = document.querySelector(".slider");     
-//   let timer;    
+  //   useEffect(()=>{
+  //   let slider = document.querySelector(".slider");
+  //   let timer;
 
-// window.addEventListener('load resize',function(){         
-//     clearTimeout( timer );          
-//     timer = setTimeout( sliderResponsive(), 100 ); 
-// })       
-// function sliderResponsive(){         
-//     if(window.innerWidth <= 420){             
-//         slider.filter('.slick-initialized').slick('unslick');         
-//     }else{             
-//         slider.not('.slick-initialized').slick({                
-//             arrows: true,                
-//             slidesToShow: 1,                 
-//             slidesToScroll: 1            
-//         });                         
-//     }       
-// }
-//   },[])
+  // window.addEventListener('load resize',function(){
+  //     clearTimeout( timer );
+  //     timer = setTimeout( sliderResponsive(), 100 );
+  // })
+  // function sliderResponsive(){
+  //     if(window.innerWidth <= 420){
+  //         slider.filter('.slick-initialized').slick('unslick');
+  //     }else{
+  //         slider.not('.slick-initialized').slick({
+  //             arrows: true,
+  //             slidesToShow: 1,
+  //             slidesToScroll: 1
+  //         });
+  //     }
+  // }
+  //   },[])
 
-// settings: "unslick"
+  // settings: "unslick"
+
   return (
     <div className="home-slick-div">
       <div id="home-container">
         <div id="home-container2">
           <Slider className="home-main-slick" {...settings2} ref={slider}>
-            <div className="home-box one"><div><HomeAboutUs /></div></div>
-            <div className="home-box two"><div className="home-slick-position1"><HomePortfolio /></div></div>
-            <div className="home-box three" ref={page3}><div className="home-slick-position2"><HomeContactUs /></div></div>
+            <div className="home-box one">
+              <div>
+                <HomeAboutUs />
+              </div>
+            </div>
+            <div className="home-box two">
+              <div className="home-slick-position1">
+                <HomePortfolio />
+              </div>
+            </div>
+            <div className="home-box three">
+              {/* ref={page3} */}
+              <div className="home-slick-position2">
+                <HomeContactUs />
+              </div>
+            </div>
           </Slider>
         </div>
       </div>
       {/** 🌼 스크롤 이미지(?) 사라짐 확인 😁 */}
       <div className="home-arrow-container">
-      <div class="center-con">
-        <div class="round">
+        <div class="center-con">
+          <div class="round">
             <div id="cta">
-              <button onClick={()=> scrollToSection(page3)} className="asd" >3페이지</button>
-            <div className="home-arrow-title">Scroll</div>
-                <span class="arrow primera next "></span>
-                <span class="arrow segunda next "></span>
-                <div className="homecontactus-cp">
-                  <p className="homecontactus-cp-p">© Copyright 2022 Today Design Company. All Rights Reserved.</p>
-                </div>
+              {/* <button onClick={() => scrollToSection(page3)} className="asd">
+                3페이지
+              </button> */}
+              <div className="home-arrow-title">Scroll</div>
+              <span class="arrow primera next "></span>
+              <span class="arrow segunda next "></span>
+              <div className="homecontactus-cp">
+                <p className="homecontactus-cp-p">
+                  © Copyright 2022 Today Design Company. All Rights Reserved.
+                </p>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
         {/* <div className="home-scroll-txt"></div>
         <div className="home-arrow"></div> */}
-        </div>
-        
-      <img src={require("../img/logo_white.png")} className="home-logo" width="230" alt="" />
-      
+      </div>
+
+      <img
+        src={require("../img/logo_white.png")}
+        className="home-logo"
+        width="230"
+        alt=""
+      />
+
       <div id="outer-container">
-      <HomeNavbar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
-        <div id="page-wrap">
-        </div>
+        <HomeNavbar
+          pageWrapId={"page-wrap"}
+          outerContainerId={"outer-container"}
+        />
+        <div id="page-wrap"></div>
       </div>
     </div>
   );
-}
+};
 
 export default Home;
